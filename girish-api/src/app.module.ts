9ceb253b76ad05e69504a5a6921086277d111d90
girishverma032 <girishverma032@gmail.com>
2022-05-13 15:28:39 +0530

    first

--- girish-api/src/app.module.ts
+++ girish-api/src/app.module.ts
@@ -1,10 +1,9 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
-import { MongooseModule } from '@nestjs/mongoose';
 
 @Module({
-  imports: [MongooseModule.forRoot('mongodb://localhost/girishdb')],
+  imports: [],
   controllers: [AppController],
   providers: [AppService],
 })
